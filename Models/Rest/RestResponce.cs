78f63fdfbada43c823edf313d61cd74636c71c34
Dmytro <dberezhkov2016@gmail.com>
2025-09-27 22:53:04 +0300

    Added REST

---
+++ Models/Rest/RestResponce.cs
@@ -0,0 +1,10 @@
+﻿namespace Coursework.Models.Rest
+{
+    public class RestResponce
+    {
+        public RestStatus Status { get; set; } = new();
+        public RestMeta Meta { get; set; } = new();
+
+        public Object? Data { get; set; }
+    }
+}
---
+++ Models/Rest/RestStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace Coursework.Models.Rest
+{
+    public class RestStatus
+    {
+        public String Phrase { get; set; } = "Ok";
+        public int Code { get; set; } = 200;
+        public bool IsOk { get; set; } = true;
+
+        public static readonly RestStatus Status200 = new() { IsOk = true, Code = 200, Phrase = "OK" };
+        public static readonly RestStatus Status400 = new() { IsOk = false, Code = 400, Phrase = "Bad Request" };
+        public static readonly RestStatus Status401 = new() { IsOk = false, Code = 401, Phrase = "Unauthorized" };
+        public static readonly RestStatus Status403 = new() { IsOk = false, Code = 403, Phrase = "Forbidden access" };
+        public static readonly RestStatus Status404 = new() { IsOk = false, Code = 404, Phrase = "Not Found" };
+    }
+}
